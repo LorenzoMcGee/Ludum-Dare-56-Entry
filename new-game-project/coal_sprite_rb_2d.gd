04a3d9b16f136f604936f0bff9fc162437148a22
Johanner Corrales <83975989+LorenzoMcGee@users.noreply.github.com>
2024-10-05 11:26:57 -0400

    Minor repairs on the projectile code and walking cycle re-introduced

--- new-game-project/coal_sprite_rb_2d.gd
+++ new-game-project/coal_sprite_rb_2d.gd
@@ -1,9 +1,15 @@
 extends RigidBody2D
-var multiplier = 25000 
-var spinning = false   
-var inMotion = false
+var multiplier = 150 
+var projectile = false
+var spawnPoint
+
+var rightDistance
+var leftDistance
+
+var walking = true
 var CoalSpriteAnim2D
 var CollShape2D
+var toggleDirection = false
 
 func closeNuff(inp,comp,threshold):
 	return abs(inp-comp) < threshold
@@ -16,6 +22,9 @@ func closeNuffVecs(inpVec,compVec,threshold):
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	rightDistance = 300
+	leftDistance = 200
+	spawnPoint = position
 	CoalSpriteAnim2D = $"CollisionShape2D/CoalSprite"
 	CollShape2D = $"CollisionShape2D"
 	CoalSpriteAnim2D.play()
@@ -24,48 +33,60 @@ func _ready() -> void:
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	
-	
-	
-	if Input.is_action_just_pressed("Space"):
-		linear_velocity  = (Vector2.RIGHT*multiplier*3.0*delta)+(Vector2.UP*multiplier*3.0*delta)
-		spinning = true
-		inMotion = true
+func _physics_process(delta: float) -> void:
+	var grounded = closeNuff(linear_velocity.y,0,0.05)
+	var velocity
+	#walk mode by default
+	if(walking):
 		rotation = 0
 		angular_velocity = 0
-		
-	if linear_velocity.y < 0:
+		if Input.is_action_just_pressed("Space"):
+			linear_velocity  = (Vector2.RIGHT*multiplier*3.0)+(Vector2.UP*multiplier*3.0)
+			walking = false
+			projectile = true
+			rotation = 0
+			angular_velocity = 0
+		elif(grounded):
+			
+			#walking code
+			if((position.x < spawnPoint.x - leftDistance and !toggleDirection) or (position.x > spawnPoint.x + rightDistance and toggleDirection)):
+				toggleDirection = !toggleDirection
+			if(!toggleDirection):
+				velocity = Vector2.LEFT * multiplier 
+				CoalSpriteAnim2D.flip_h = false
+			else:
+				velocity = Vector2.RIGHT * multiplier 
+				CoalSpriteAnim2D.flip_h = true
+			linear_velocity = velocity
+			print("CODE RUNNING ",position,velocity)
+			
+	#could also act as a projectile
+	elif(projectile):
 		lock_rotation = false
-	#print(spinning," ",inMotion," ",rotation)
-	#print(linear_velocity,angular_velocity,":",spinning,":",CoalSpriteAnim2D.rotation,":",CollShape2D.rotation,":",closeNuff(angular_velocity,0,3),":",closeNuffVecs(linear_velocity,Vector2.ZERO,3),":",angular_velocity)
+		#need to detect if the coalSprite is on the ground
+		if(closeNuff(angular_velocity,0,0.2) and grounded):
+			walking = true
+			projectile = false
+			linear_velocity = Vector2.UP*multiplier*2   
+			lock_rotation = true
+			rotation = 0
+			angular_velocity = 0
+			
+	else:
+		pass
 	
-	if(spinning and closeNuff(angular_velocity,0.5,0.1)  and abs(angular_velocity) > 0  ):
-		spinning = false
-		linear_velocity = Vector2.UP*delta*multiplier
-	elif(not spinning and inMotion):
-		angular_velocity = 0
-		rotation = 0
-		inMotion = false
-	elif(not inMotion):
-		angular_velocity = 0
-		lock_rotation = true
-		rotation = 0
-		print("test")
-		
+	
+	print(linear_velocity," ",angular_velocity," ",rotation," ",walking," ",projectile)
+	
+	
+	
+	
 	
 	
 	"""
 	match animationIndex:
 		0:
-			if(position.x < 200 or position.x > 1057):
-				toggleDirection *= -1
-			if(toggleDirection == 1):
-				velocity = Vector2.LEFT * multiplier * delta
-				flip_h = false
-			else:
-				velocity = Vector2.RIGHT * multiplier * delta 
-				flip_h = true
+			
 		_:
 			pass
 	

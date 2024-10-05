ecd9ed2a354f4e4711fb21d2497fbc1515bd7b1f
Johanner Corrales <83975989+LorenzoMcGee@users.noreply.github.com>
2024-10-04 20:50:56 -0400

    StoneTile Added

---
+++ new-game-project/coal_sprite.gd
@@ -0,0 +1,30 @@
+extends AnimatedSprite2D
+var toggleDirection = 1
+var multiplier = 140
+var velocity
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	play()
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	
+	if(position.x < 200 or position.x > 1057):
+		toggleDirection *= -1
+	
+	if(toggleDirection == 1):
+		velocity = Vector2.LEFT * multiplier * delta
+		flip_h = false
+	else:
+		velocity = Vector2.RIGHT * multiplier * delta 
+		flip_h = true
+	
+	
+	
+	position += velocity
+	
+	
+	pass
